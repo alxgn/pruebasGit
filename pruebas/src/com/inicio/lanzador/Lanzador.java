b125113ac2818f676025c0eb4c6b436b49f1c18d
Alxgn <eisenstard@gmail.com>
2021-12-10 22:18:10 +0100

    Hola mundo

---
+++ pruebas/src/com/inicio/lanzador/Lanzador.java
@@ -0,0 +1,10 @@
+package com.inicio.lanzador;
+
+public class Lanzador {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo");
+	}
+
+}
